0ff03a7af05cb158cba315e0b60c0dbf8229d1e6
sadat.malik <sadat.malik@me.com>
2022-03-16 14:07:22 +0000

    abstract factory

---
+++ src/main/java/com/sadatmalik/factories/abstractfactory/AbstractFactory.java
@@ -0,0 +1,104 @@
+package com.sadatmalik.factories.abstractfactory;
+
+import com.sadatmalik.unittesting.UnitTest;
+
+/**
+ * @author sm@creativefusion.net
+ */
+interface Obstacle {
+    void action();
+}
+
+interface Player {
+    void interactWith(Obstacle o);
+}
+
+class Kitty implements Player {
+    public void interactWith(Obstacle ob) {
+        System.out.print("Kitty has encountered a ");
+        ob.action();
+    }
+}
+
+class KungFuGuy implements Player {
+    public void interactWith(Obstacle ob) {
+        System.out.print("KungFuGuy now battles a ");
+        ob.action();
+    }
+}
+
+class Puzzle implements Obstacle {
+    public void action() {
+        System.out.println("Puzzle");
+    }
+}
+
+class NastyWeapon implements Obstacle {
+    public void action() {
+        System.out.println("NastyWeapon"); }
+}
+
+// The Abstract Factory:
+interface GameElementFactory {
+    Player makePlayer();
+    Obstacle makeObstacle();
+}
+
+// Concrete factories:
+class KittiesAndPuzzles implements GameElementFactory {
+    public Player makePlayer() {
+        return new Kitty();
+    }
+    public Obstacle makeObstacle() {
+        return new Puzzle();
+    }
+}
+
+class KillAndDismember implements GameElementFactory {
+    public Player makePlayer() {
+        return new KungFuGuy();
+    }
+    public Obstacle makeObstacle() {
+        return new NastyWeapon();
+    }
+}
+
+class GameEnvironment {
+    private GameElementFactory gef;
+    private Player p;
+    private Obstacle ob;
+
+    public GameEnvironment(GameElementFactory factory) {
+        gef = factory;
+        p = factory.makePlayer();
+        ob = factory.makeObstacle();
+    }
+
+    public void play() {
+        p.interactWith(ob);
+    }
+}
+
+public class AbstractFactory extends UnitTest {
+    GameElementFactory
+            kp = new KittiesAndPuzzles(),
+            kd = new KillAndDismember();
+
+    GameEnvironment
+            g1 = new GameEnvironment(kp),
+            g2 = new GameEnvironment(kd);
+
+    public void test1() {
+        g1.play();
+    }
+
+    public void test2() {
+        g2.play();
+    }
+
+    public static void main(String args[]) {
+        AbstractFactory g = new AbstractFactory();
+        g.test1();
+        g.test2();
+    }
+}
